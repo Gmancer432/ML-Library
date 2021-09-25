93d2b1905c729b5a144dc6d09291a33b8f644d9a
Gmancer432 <sean@richens.com>
2021-09-25 00:04:20 -0600

    Started setting up files for the ID3 decision tree classifier

---
+++ ID3.py
@@ -0,0 +1,19 @@
+
+# ID3.py
+# Created by Sean Richens for UofU CS5350
+# --------
+# Contains methods for constructing and using a decision tree.
+
+
+class DTNode:
+	def __init__(self):
+		
+		
+
+
+class DecisionTree:
+	def __init__(self):
+		root = None
+
+
+def ID3
